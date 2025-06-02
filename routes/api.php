dd070633db9a285c6dff5645025ec54e5af3250e
kevinchoo96 <75832420+kevinchoo96@users.noreply.github.com>
2025-06-02 17:25:51 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/usersss', function (Request $request) {
+    dd(User::find(1));
+});
